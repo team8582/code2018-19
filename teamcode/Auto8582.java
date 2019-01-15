fd32d12165299b650c9276fcf8903afec8179c33
team8582 <46727294+team8582@users.noreply.github.com>
2019-01-15 15:49:25 -0500

    Add files via upload

---
+++ teamcode/Auto8582.java
@@ -0,0 +1,228 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.internal.opmode.TelemetryImpl;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Auto8582")
+public class Auto8582 extends LinearOpMode {
+    Hardware8582 robot = new Hardware8582();
+    private ElapsedTime runtime = new ElapsedTime();       //Create variable to keep track of elapsed time.
+    static final double DRIVE_SPEED = 0.7;
+    static final double TURN_SPEED = 0.5;
+    final double COUNTS_PER_MOTOR_REV = 1120;     //Andymark Motor Encoder
+    final double DRIVE_GEAR_REDUCTION = 1.0;      //This is < 1.0 if geared up
+    final double WHEEL_DIAMETER_INCHES = 3.75;     //For figuring circumference
+    final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION)*
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+        //turn on encoders
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.frontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.backDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        idle();
+
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.backDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        lift(DRIVE_SPEED, -.4, 10);
+        frontBackDrive(DRIVE_SPEED, -.05, -.05, 2);
+        sideDrive(DRIVE_SPEED,.5,.5,10);
+        /*robot.corral.setPower(-.5);
+        sleep(300);
+*/
+        idle();
+
+
+    }
+
+    public void frontBackDrive(double speed, double leftInches, double rightInches, double timeoutS) throws InterruptedException {
+        int newLeftTarget; //declare variable for the target position of the left motor
+        int newRightTarget; //declare variable for the target position of the right motor
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = robot.frontDrive.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH); //initializes newLeftTarget to represent the desired distance travelled by the left motors
+            newRightTarget = robot.backDrive.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH); //initializes newRightTarget to represent the desired distance travelled by the right motors
+
+            //sets the motors to run to the position corresponding to their side (left or right)
+            robot.frontDrive.setTargetPosition(newLeftTarget);
+            robot.backDrive.setTargetPosition(newRightTarget);
+
+
+            // Turn On RUN_TO_POSITION
+            robot.frontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.backDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.frontDrive.setPower(Math.abs(speed));
+            robot.backDrive.setPower(Math.abs(speed));
+
+
+
+            // keep looping while still active, and there is time left, and both motors are running.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.backDrive.isBusy() && robot.frontDrive.isBusy())) {
+
+                // Display postion on path to the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        robot.frontDrive.getCurrentPosition(),
+                        robot.backDrive.getCurrentPosition());
+                telemetry.update();
+
+                // Allow time for other processes to run.
+                //idle();
+            }
+
+            // Stop all motion;
+            robot.backDrive.setPower(0);
+            robot.frontDrive.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            robot.backDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.frontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+            sleep(200);   // optional pause after each move
+        }
+    }
+
+    public void sideDrive(double speed, double leftInches, double rightInches, double timeoutS) throws InterruptedException {
+        int newLeftTarget; //declare variable for the target position of the left motor
+        int newRightTarget; //declare variable for the target position of the right motor
+
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = robot.leftDrive.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH); //initializes newLeftTarget to represent the desired distance travelled by the left motors
+            newRightTarget = robot.rightDrive.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH); //initializes newRightTarget to represent the desired distance travelled by the right motors
+
+            //sets the motors to run to the position corresponding to their side (left or right)
+            robot.leftDrive.setTargetPosition(newRightTarget);
+            robot.rightDrive.setTargetPosition(newRightTarget);
+
+
+            // Turn On RUN_TO_POSITION
+            robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.leftDrive.setPower(Math.abs(speed));
+            robot.rightDrive.setPower(Math.abs(speed));
+
+
+
+            // keep looping while still active, and there is time left, and both motors are running.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.backDrive.isBusy() && robot.frontDrive.isBusy())) {
+
+                // Display postion on path to the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        robot.leftDrive.getCurrentPosition(),
+                        robot.rightDrive.getCurrentPosition());
+                telemetry.update();
+
+                // Allow time for other processes to run.
+                //idle();
+            }
+
+            // Stop all motion;
+            robot.leftDrive.setPower(0);
+            robot.rightDrive.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+            sleep(200);   // optional pause after each move
+        }
+    }
+
+    public void lift(double speed, double inches, double timeoutS)
+    {
+        int target;
+
+
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            target = robot.lift.getCurrentPosition() + (int) (inches * COUNTS_PER_INCH); //initializes target to represent the desired distance travelled by the lift motor
+
+            //sets the motor to run to the position
+            robot.lift.setTargetPosition(target);
+
+
+            // Turn On RUN_TO_POSITION
+            robot.lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.lift.setPower(Math.abs(speed));
+
+
+
+            // keep looping while still active, and there is time left, and both motors are running.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.lift.isBusy())) {
+
+                // Display postion on path to the driver.
+                telemetry.addData("Path1", "Running to %7d", target);
+                telemetry.addData("Path2", "Running at %7d", robot.lift.getCurrentPosition());
+                telemetry.update();
+
+                // Allow time for other processes to run.
+                //idle();
+            }
+
+            // Stop all motion;
+            robot.lift.setPower(0);
+
+
+            // Turn off RUN_TO_POSITION
+            robot.lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+            sleep(200);   // optional pause after each move
+        }
+
+    }
+
+}
---
+++ teamcode/Hardware8582.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+public class Hardware8582 {
+    public DcMotor leftDrive = null;
+    public DcMotor rightDrive = null;
+    public DcMotor frontDrive = null;
+    public DcMotor backDrive= null;
+
+
+    public DcMotor lift = null;
+
+    //public CRServo corral = null;
+
+
+    HardwareMap hwMap8582 = null;
+
+
+    private ElapsedTime period = new ElapsedTime();
+
+    public Hardware8582(){
+
+    }
+
+    public void init(HardwareMap ahwMap) {
+        hwMap8582 = ahwMap;
+
+        leftDrive = hwMap8582.get(DcMotor.class, "leftDrive");
+        rightDrive = hwMap8582.get(DcMotor.class, "rightDrive");
+        frontDrive = hwMap8582.get(DcMotor.class, "frontDrive");
+        backDrive = hwMap8582.get(DcMotor.class, "backDrive");
+        lift = hwMap8582.get(DcMotor.class, "lift");
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontDrive.setDirection(DcMotor.Direction.REVERSE);
+        backDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        lift.setDirection(DcMotor.Direction.REVERSE);
+
+        //corral = hwMap8582.get(CRServo.class, "corral");
+        //corral.setPower(0.0);
+
+    }
+
+    /***
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs Length of wait cycle in mSec.
+     * @throws InterruptedException
+     */
+    public void waitForTick (long periodMs) throws InterruptedException {
+
+        long remaining = periodMs - (long)period.milliseconds();
+        if (remaining > 0)
+            Thread.sleep(remaining);
+        period.reset();
+    }
+}
